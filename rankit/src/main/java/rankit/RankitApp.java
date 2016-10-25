8f90441518b1be9534fa6a6b35eb0a636bebd330
ChristianStroyer <ChristianStroyer@users.noreply.github.com>
2016-10-26 00:00:34 +0200

    Splitting functionality into a stand-alone event-engine, and a request handler, for easier testing and maintenance,

--- rankit/src/main/java/rankit/RankitApp.java
+++ rankit/src/main/java/rankit/RankitApp.java
@@ -1,29 +1,23 @@
 package rankit;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import rankit.logic.Scoring;
 import rankit.model.Match;
 import rankit.model.Player;
 import rankit.model.event.CreatePlayerEvent;
-import rankit.model.event.Event;
 import rankit.model.event.RegisterMatchEvent;
 import ro.pippo.core.Application;
 import ro.pippo.core.RedirectHandler;
 
 public class RankitApp extends Application {
-
+	
+	private RankitEngine engine = new RankitEngine();
+		
 	private Logger logger = LoggerFactory.getLogger(RankitApp.class);
-	private Map<Integer, Player> players = new HashMap<>();
-	private List<Match> matches = new ArrayList<>();
-	private boolean matches_need_sorting = false;
 	private DatabaseUtil _db;
 
 	public RankitApp(DatabaseUtil db) {
@@ -49,112 +43,55 @@ public class RankitApp extends Application {
         GET("/", new RedirectHandler("/index.html"));
 
 		GET("/api/list", (routeContext) -> {
-			routeContext.json().send(getSortedPlayerlist());
+			routeContext.json().send(engine.getSortedPlayerlist());
 		});
 
 		POST("/api/player", (routeContext) -> {
+
 			Player newPlayer = routeContext.createEntityFromBody(Player.class);
-			if (newPlayer != null) {
-				newPlayer.setId(findUnusedPlayerId());
-				addNewPlayer(newPlayer);
-				_db.saveEvent(new CreatePlayerEvent(newPlayer.getId(), newPlayer.getName()));
-				routeContext.json().send(getSortedPlayerlist());
-			} else {
+			if (newPlayer == null) {
 				routeContext.status(501).send("Attribute name is undefined");
+				return;			
 			}
+			
+			CreatePlayerEvent createPlayerEvent = engine.newAndApplyCreatePlayerEvent( newPlayer.getName() );
+			_db.saveEvent(createPlayerEvent);
+			
+			routeContext.json().send(engine.getSortedPlayerlist());
+
 		});
 		
 		GET("/api/match", (routeContext) -> {
-			sortMatchesIfNeeded();
-			List<Match> latestMatches = matches.subList( 0, Math.min(matches.size(), 100) );
-		
+			List<Match> latestMatches = engine.getSortedMatchList();
 			routeContext.json().send(latestMatches);
 		});
 
 		POST("/api/match", (routeContext) -> {
 			Match match = routeContext.createEntityFromBody(Match.class);
-			if(match != null){
-				if( match.time == null ) {
-					match.time = new Date();
-				}				
-				registerMatch(match);
-				_db.saveEvent(new RegisterMatchEvent(match));
-				routeContext.json().send(getSortedPlayerlist());
-			} else {
-				routeContext.status(501).send("Attribute match is incorrect");
+			if(match == null){ 
+				routeContext.status(501).send("Attribute match is incorrect");			
 			}
+			
+			if( match.time == null ) {
+				match.time = new Date();
+			}				
+			
+			RegisterMatchEvent registerMatchEvent = engine.newAndApplyRegisterMatchEvent(match);
+			_db.saveEvent(registerMatchEvent);
+			
+			routeContext.json().send(engine.getSortedPlayerlist());
 
 		});
-	}
-
-	private List<Player> getSortedPlayerlist() {
-		List<Player> playerList = new ArrayList<Player>(players.values());
-		playerList.sort((p1, p2) -> p2.getPoints() - p1.getPoints());
-		return playerList;
 	}
 
 	@Override
 	protected void onDestroy() {
 		_db.close();
 	}
-
-	private void addNewPlayer(Player newPlayer) {
-		newPlayer.addPoints(1000);
-		players.put(newPlayer.getId(), newPlayer);
-	}
-
-	private void registerMatch(Match match) {
-		Scoring.score(match, players);
-		matches.add(match);
-		matches_need_sorting = true;
-	}
-
-	// Synchronized to avoid competetive sorting
-	private synchronized void sortMatchesIfNeeded() {
-		if(matches_need_sorting == false)
-			return;				
-		matches.sort( (o1, o2) -> o2.time.compareTo(o1.time));		
-		matches_need_sorting = false;
-	}
 	
 	private void initList() {
-		applyEvents(_db.getEvents());
+		engine.applyEvents(_db.getEvents());
 	}
 
-	private void applyEvents(List<Event> list) {
-		for (Event event : list) {
-			if (event instanceof CreatePlayerEvent) {
-				CreatePlayerEvent playerEvent = ((CreatePlayerEvent) event);
-				addNewPlayer(new Player(playerEvent.getPlayerName(), playerEvent.getPlayerId()));
-			} else if (event instanceof RegisterMatchEvent) {
-				Match match = ((RegisterMatchEvent) event).getMatch();
-				if( match.time == null ) {
-					match.time = event.getEventTime();
-				}
-				registerMatch(match);
-			} else {
-				logger.error("Unhandled event i applyEvents " + event.getClass().getName());
-			}
-		}
-	}
 	
-	private synchronized int findUnusedPlayerId() {
-		int tryThisId = (int) Math.floor( Math.random()*100000000+100 );
-		
-		boolean playerExists = true;
-		
-		for(int i=0;i<100;i++) {
-			playerExists = players.containsKey(new Integer(tryThisId));
-			if(!playerExists) {
-				break;
-			} else {
-				tryThisId++;
-			}
-		}
-		
-		if(playerExists)
-			throw new RuntimeException("Couldn't find unused playerId!");
-		
-		return tryThisId;
-	}
 }
